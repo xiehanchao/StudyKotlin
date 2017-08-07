0a37863836b057db09af2035c3486ec17dd722cc
xiehanchao <1063271662@qq.com>
2017-08-07 10:24:12 +0800

    Update MainActivity.java

--- StudyKotlin/app/src/main/java/com/example/mac/studykotlin/MainActivity.java
+++ StudyKotlin/app/src/main/java/com/example/mac/studykotlin/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String s="111";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
