1693fd1055d3b16ab1b31028ca9b7753c2f1af17
xiehanchao <1063271662@qq.com>
2017-08-07 09:58:00 +0800

    add files

---
+++ StudyKotlin/app/src/main/java/com/example/mac/studykotlin/myView.java
@@ -0,0 +1,26 @@
+package com.example.mac.studykotlin;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by MAC on 2017/8/4.
+ */
+
+public class myView extends View {
+
+    public myView(Context context) {
+        super(context);
+    }
+
+    public myView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public myView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
