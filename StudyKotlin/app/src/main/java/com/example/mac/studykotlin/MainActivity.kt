ce9b9834e97bb625d97bb5ed84b9b096d8f9d34c
xiehanchao <1063271662@qq.com>
2017-08-11 00:00:00 +0800

    Update MainActivity.kt

--- StudyKotlin/app/src/main/java/com/example/mac/studykotlin/MainActivity.kt
+++ StudyKotlin/app/src/main/java/com/example/mac/studykotlin/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+qqq
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
